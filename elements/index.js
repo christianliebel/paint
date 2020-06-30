1b5b5fd103fed73f44b34e293c782b707554f4df
Christian Liebel <christian.liebel@thinktecture.com>
2020-06-30 18:09:19 +0200

    feat: add inset container

---
+++ elements/index.js
@@ -0,0 +1,1 @@
+import './inset-container.js';
---
+++ elements/inset-container.js
@@ -0,0 +1,22 @@
+import {html, LitElement} from '../web_modules/lit-element.js';
+import {css} from "../web_modules/lit-element";
+
+class InsetContainer extends LitElement {
+    static get styles() {
+        return css`
+            :host {
+                border: 1px solid var(--canvas);
+                border-bottom-color: var(--highlight-text);
+                border-right-color: var(--highlight-text);
+                
+                display: flex;
+                align-items: flex-end;
+            }
+        `;
+    }
+
+    render() {
+        return html`<slot></slot>`;
+    }
+}
+customElements.define('paint-inset-container', InsetContainer);
