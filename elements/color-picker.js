90a941c050b1387abd0fb2f9f13235426af98774
Christian Liebel <christian.liebel@thinktecture.com>
2020-07-02 21:08:23 +0200

    feat(elements): add color picker

---
+++ elements/color-picker.js
@@ -0,0 +1,52 @@
+import {css, html, LitElement} from '../web_modules/lit-element.js';
+
+class ColorPicker extends LitElement {
+    static get properties() {
+        return {
+            color: {type: String}
+        };
+    }
+
+    static get styles() {
+        return css`
+            :host {
+                border: 1px solid var(--highlight-text);
+                border-top-color: var(--canvas);
+                border-left-color: var(--canvas);
+            }
+            
+            div.frame {
+                box-sizing: border-box;
+                border: 1px solid var(--button-face);
+                border-top-color: var(--button-text);
+                border-left-color: var(--button-text);
+                height: 100%;
+            }
+        `;
+    }
+
+    constructor() {
+        super();
+        this.addEventListener('click', () => {
+            this.dispatchColorEvent('primary');
+        });
+        this.addEventListener('contextmenu', event => {
+            this.dispatchColorEvent('secondary');
+            event.preventDefault();
+        })
+    }
+
+    dispatchColorEvent(type) {
+        this.dispatchEvent(new CustomEvent(`${type}-color-selected`, {
+            detail: {value: this.color},
+            bubbles: true,
+            composed: true
+        }));
+    }
+
+    render() {
+        return html`<div class="frame" style="background-color: ${this.color || 'hotpink'};"></div>`;
+    }
+}
+
+customElements.define('paint-color-picker', ColorPicker);
--- elements/index.js
+++ elements/index.js
@@ -1,5 +1,6 @@
 import './app.js';
 import './canvas.js';
+import './color-picker.js';
 import './handle.js';
 import './inset-container.js';
 import './menu-bar.js';
