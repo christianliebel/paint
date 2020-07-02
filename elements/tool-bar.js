5f9ff9ed3e3daf4e60ad81715465a35e70c172c1
Christian Liebel <christian.liebel@thinktecture.com>
2020-07-02 21:07:03 +0200

    feat(elements): slot for toolbar

--- elements/tool-bar.js
+++ elements/tool-bar.js
@@ -3,12 +3,16 @@ import {css, html, LitElement} from '../web_modules/lit-element.js';
 class ToolBar extends LitElement {
     static get styles() {
         return css`
-            :host { background-color: var(--button-face); }
+            :host {
+                background-color: var(--button-face);
+                padding-right: 1px;
+            }
         `;
     }
 
     render() {
-        return html`(Tool)`;
+        return html`<slot></slot>`;
     }
 }
+
 customElements.define('paint-tool-bar', ToolBar);
