136119e63649320f94060841dc9f529016ace1ee
Christian Liebel <christian.liebel@thinktecture.com>
2020-07-03 19:57:36 +0200

    feat(actions): new action interface

--- actions/open.js
+++ actions/open.js
@@ -1,11 +1,13 @@
 import {fileOpen} from '../web_modules/browser-nativefs.js';
 
-export async function open(canvas, context) {
+export async function open({ canvas, context }) {
     const file = await fileOpen({extensions: ['png']});
     const image = new Image();
     image.onload = () => {
         canvas.width = image.width;
         canvas.height = image.height;
+        context.fillStyle = 'white';
+        context.fillRect(0, 0, image.width, image.height);
         context.drawImage(image, 0, 0);
         URL.revokeObjectURL(image.src);
     };
