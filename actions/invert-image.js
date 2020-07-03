f1ab65b163a836f54e6f580126824d4c1c268986
Christian Liebel <christian.liebel@thinktecture.com>
2020-07-03 21:44:00 +0200

    feat(actions): implement invert image

---
+++ actions/invert-image.js
@@ -0,0 +1,7 @@
+export function invertImage({ canvas, context }) {
+    const previousCompositeOperation = context.globalCompositeOperation;
+    context.globalCompositeOperation = 'difference';
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.globalCompositeOperation = previousCompositeOperation;
+}
