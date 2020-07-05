ca1a2234f6f22d9dc4f862db60574f38a9df665b
Christian Liebel <christian.liebel@thinktecture.com>
2020-07-05 13:26:31 +0200

    feat(tools): use bresenham line algorithm
    Closes #70

--- tools/pencil.js
+++ tools/pencil.js
@@ -1,12 +1,16 @@
+import bresenhamLine from '../web_modules/bresenham-line.js';
+
 export class PencilTool {
   onMouseDown({ event, context, x, y, secondaryColor, primaryColor }) {
-    context.beginPath();
-    context.strokeStyle = event.button === 2 ? secondaryColor : primaryColor;
-    context.moveTo(x, y);
+    context.fillStyle = event.button === 2 ? secondaryColor : primaryColor;
+    context.fillRect(x, y, 1, 1);
+    this.previous = { x, y };
   }
 
   onMouseMove({ x, y, context }) {
-    context.lineTo(x, y);
-    context.stroke();
+    for (let point of bresenhamLine(this.previous, { x, y })) {
+      context.fillRect(point.x, point.y, 1, 1);
+    }
+    this.previous = { x, y };
   }
 }
