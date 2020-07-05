8c226a161922b357059d2f2d572a98b1283b6db7
Christian Liebel <christian.liebel@thinktecture.com>
2020-07-05 11:58:58 +0200

    chore: configure prettier and eslint
    Closes #56
    Closes #32

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {},
+};
--- actions/copy.js
+++ actions/copy.js
@@ -1,4 +1,7 @@
-export function copy({canvas}) {
-    // TODO: Copy selection
-    canvas.toBlob(blob => navigator.clipboard.write([new ClipboardItem({[blob.type]: blob})]));
+export function copy({ canvas }) {
+  // TODO: Copy selection
+  canvas.toBlob((blob) =>
+    // eslint-disable-next-line no-undef
+    navigator.clipboard.write([new ClipboardItem({ [blob.type]: blob })]),
+  );
 }
