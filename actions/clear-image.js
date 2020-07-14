3870b44ab42dfe0fd6eab95f53661a64b6c157fa
Christian Liebel <christian.liebel@thinktecture.com>
2020-07-14 12:04:59 +0200

    feat(actions): clear image with secondary color

--- actions/clear-image.js
+++ actions/clear-image.js
@@ -1,4 +1,5 @@
-export function clearImage({ canvas, context }) {
-  context.fillStyle = 'white';
+// TODO: When is it disabled?
+export function clearImage({ canvas, context, secondaryColor }) {
+  context.fillStyle = secondaryColor;
   context.fillRect(0, 0, canvas.width, canvas.height);
 }
