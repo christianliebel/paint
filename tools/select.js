8d7e0cf51dea6075fca42e8dd9af52f8fb897238
Christian Liebel <christian.liebel@thinktecture.com>
2020-07-14 12:03:56 +0200

    feat(tools): preliminary select tool

--- tools/select.js
+++ tools/select.js
@@ -1,15 +1,31 @@
 export class SelectTool {
-  onPointerDown({ x, y }) {
+  onPointerDown({ previewContext, x, y }) {
     this.startPosition = { x, y };
+
+    previewContext.setLineDash([4]);
   }
 
-  onPointerMove({ canvas, previewContext, x, y }) {
+  onPointerMove({ element, canvas, previewContext, x, y }) {
+    // TODO: Scope to canvas
     previewContext.clearRect(0, 0, canvas.width, canvas.height);
     previewContext.strokeRect(this.startPosition.x + 0.5, this.startPosition.y + 0.5, x - this.startPosition.x, y - this.startPosition.y);
+
+    element.dispatchEvent(new CustomEvent('area', {
+      detail: {
+        width: Math.abs(x - this.startPosition.x),
+        height: Math.abs(y - this.startPosition.y),
+      },
+      bubbles: true,
+      composed: true,
+    }));
   }
 
-  onPointerUp({ canvas, selection, previewContext, x, y }) {
+  onPointerUp({ canvas, element, selection, previewContext, x, y }) {
+    previewContext.setLineDash([]);
     previewContext.clearRect(0, 0, canvas.width, canvas.height);
+
+    element.dispatchEvent(new CustomEvent('area', { bubbles: true, composed: true }));
+
     selection.x = this.startPosition.x;
     selection.y = this.startPosition.y;
     selection.width = x - this.startPosition.x;
