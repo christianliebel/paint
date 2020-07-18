06b6f55c2db1161ee44fe92c451709699c7e964f
Christian Liebel <christian.liebel@thinktecture.com>
2020-07-18 13:38:55 +0200

    feat(tool): implement line tool
    Closes #23

--- tools/line.js
+++ tools/line.js
@@ -14,11 +14,31 @@ export class LineTool {
     this.drawLine(x, y, context, previewContext, canvas, lineWidth);
   }
 
-  drawLine(x, y, targetContext, previewContext, canvas) {
-    // TODO: Midpoint, circle, line width
+  drawLine(x, y, targetContext, previewContext, canvas, lineWidth) {
     previewContext.clearRect(0, 0, canvas.width, canvas.height);
     for (let point of bresenhamLine({ x, y }, this.startPosition)) {
-      targetContext.fillRect(point.x, point.y, 1, 1);
+      if (lineWidth === 1) {
+        targetContext.fillRect(point.x, point.y, 1, 1);
+      }
+
+      if (lineWidth === 2) {
+        targetContext.fillRect(point.x - 1, point.y - 1, 2, 2);
+      }
+
+      if (lineWidth === 3) {
+        targetContext.fillRect(point.x - 1, point.y, 3, 1);
+        targetContext.fillRect(point.x, point.y - 1, 1, 3);
+      }
+
+      if (lineWidth === 4) {
+        targetContext.fillRect(point.x - 1, point.y - 2, 2, 4);
+        targetContext.fillRect(point.x - 2, point.y - 1, 4, 2);
+      }
+
+      if (lineWidth === 5) {
+        targetContext.fillRect(point.x - 1, point.y - 2, 3, 5);
+        targetContext.fillRect(point.x - 2, point.y - 1, 5, 3);
+      }
     }
   }
 }
