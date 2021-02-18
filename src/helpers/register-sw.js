c1d43ba53e531a0376567f631e550744c8257d1f
Christian Liebel <christian.liebel@thinktecture.com>
2021-02-18 16:24:17 +0100

    build: only register SW for prod builds

--- src/helpers/register-sw.js
+++ src/helpers/register-sw.js
@@ -1,4 +1,4 @@
-if ('serviceWorker' in navigator && !location.host.startsWith('localhost:')) {
+if (import.meta.env.MODE === 'production' && 'serviceWorker' in navigator) {
   window.addEventListener('load', () =>
     navigator.serviceWorker.register('/sw.js'),
   );
