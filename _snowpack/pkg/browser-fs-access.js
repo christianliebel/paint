902625ad53c71a356a40d1bf6afb831653d8e73f
christianliebel <christian.liebel@thinktecture.com>
2021-03-23 06:53:55 +0000

    Deploying to gh-pages from @ christianliebel/paint@c589a326b5bea10ac1aac7ff60b05f25c37af043 🚀

--- _snowpack/pkg/browser-fs-access.js
+++ _snowpack/pkg/browser-fs-access.js
@@ -1,11 +1,11 @@
 // @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
-const e="chooseFileSystemEntries"in self?"chooseFileSystemEntries":"showOpenFilePicker"in self&&"showOpenFilePicker";
+const e=(()=>{if(self!==top)try{top.location;}catch{return !1}else {if("chooseFileSystemEntries"in self)return "chooseFileSystemEntries";if("showOpenFilePicker"in self)return "showOpenFilePicker"}return !1})();
 
 // @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
-const s=e?"chooseFileSystemEntries"===e?import('./common/file-open-5eb48e45.js'):import('./common/file-open-aa0e13c8.js'):import('./common/file-open-d6183210.js');async function fileOpen(...e){return (await s).default(...e)}
+const s=e?"chooseFileSystemEntries"===e?import('./common/file-open-5eb48e45.js'):import('./common/file-open-aa0e13c8.js'):import('./common/file-open-842ac98e.js');async function fileOpen(...e){return (await s).default(...e)}
 
 // @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
-e?"chooseFileSystemEntries"===e?import('./common/directory-open-abd2ea6b.js'):import('./common/directory-open-15776d53.js'):import('./common/directory-open-7e014d09.js');
+e?"chooseFileSystemEntries"===e?import('./common/directory-open-abd2ea6b.js'):import('./common/directory-open-15776d53.js'):import('./common/directory-open-a144b74f.js');
 
 // @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
 const s$1=e?"chooseFileSystemEntries"===e?import('./common/file-save-bedc4f3a.js'):import('./common/file-save-d64fc637.js'):import('./common/file-save-f0a9af4f.js');async function fileSave(...e){return (await s$1).default(...e)}
