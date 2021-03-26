5275d7fef79636a9195aeea4f188a9278175c9fb
christianliebel <christian.liebel@thinktecture.com>
2021-03-26 06:36:16 +0000

    Deploying to gh-pages from @ christianliebel/paint@820dab19b51834057024a4289356ef510d18913c 🚀

--- _snowpack/pkg/common/directory-open-a144b74f.js
+++
@@ -1,4 +0,0 @@
-// @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
-var directoryOpen = async(e={})=>(e.recursive=e.recursive||!1,new Promise(((t,r)=>{const i=document.createElement("input");i.type="file",i.webkitdirectory=!0;const n=()=>{window.removeEventListener("focus",n),setTimeout((()=>{0===i.files.length&&r(new DOMException("The user aborted a request.","AbortError"));}),500);};i.addEventListener("click",(()=>{window.addEventListener("focus",n,!0);})),i.addEventListener("change",(()=>{let r=Array.from(i.files);e.recursive||(r=r.filter((e=>2===e.webkitRelativePath.split("/").length))),t(r);})),i.click();})));
-
-export default directoryOpen;
