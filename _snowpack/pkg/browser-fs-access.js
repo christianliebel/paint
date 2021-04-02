7a99022aade51666a0f382edd35c77cc86053977
christianliebel <christian.liebel@thinktecture.com>
2021-04-02 18:52:36 +0000

    Deploying to gh-pages from @ christianliebel/paint@7a98931f97cfd020e72593361d16aeaa0f25a454 🚀

--- _snowpack/pkg/browser-fs-access.js
+++ _snowpack/pkg/browser-fs-access.js
@@ -8,6 +8,6 @@ const s=e?"chooseFileSystemEntries"===e?import('./common/file-open-5eb48e45.js')
 e?"chooseFileSystemEntries"===e?import('./common/directory-open-abd2ea6b.js'):import('./common/directory-open-15776d53.js'):import('./common/directory-open-9af7c83d.js');
 
 // @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
-const s$1=e?"chooseFileSystemEntries"===e?import('./common/file-save-bedc4f3a.js'):import('./common/file-save-d64fc637.js'):import('./common/file-save-f0a9af4f.js');async function fileSave(...e){return (await s$1).default(...e)}
+const s$1=e?"chooseFileSystemEntries"===e?import('./common/file-save-bedc4f3a.js'):import('./common/file-save-9ed27b87.js'):import('./common/file-save-f0a9af4f.js');async function fileSave(...e){return (await s$1).default(...e)}
 
 export { fileOpen, fileSave };
---
+++ _snowpack/pkg/common/file-save-9ed27b87.js
@@ -0,0 +1,4 @@
+// @license © 2020 Google LLC. Licensed under the Apache License, Version 2.0.
+var fileSave = async(e,t={},i=null,a=!1)=>{t.fileName=t.fileName||"Untitled";const s={};if(t.mimeTypes?(t.mimeTypes.push(e.type),t.mimeTypes.map((e=>{s[e]=t.extensions||[];}))):s[e.type]=t.extensions||[],i)try{await i.getFile();}catch(e){if(i=null,a)throw e}const n=i||await window.showSaveFilePicker({suggestedName:t.fileName,types:[{description:t.description||"",accept:s}]}),l=await n.createWritable();return await l.write(e),await l.close(),n};
+
+export default fileSave;
