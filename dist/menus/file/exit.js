211001343b53672679ca39d37469fdd3b73d2f4d
christianliebel <christian.liebel@thinktecture.com>
2021-06-02 15:07:46 +0000

    Deploying to gh-pages from @ christianliebel/paint@63602a8b84dfeaedc3ac003a512bd5bbc61bee77 🚀

--- dist/menus/file/exit.js
+++ dist/menus/file/exit.js
@@ -2,7 +2,9 @@ import { handleUnsavedChanges } from '../../helpers/handle-unsaved-changes.js';
 export class ExitAction {
   async execute(drawingContext) {
     try {
-      await handleUnsavedChanges(drawingContext);
+      await handleUnsavedChanges(drawingContext); // suppress beforeunload handler
+
+      drawingContext.document.dirty = false;
       window.close();
     } catch {// Silently catch any errors
     }
