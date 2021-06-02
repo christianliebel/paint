d5dd13e48ea3e749b75cffe0f046850f8903c946
Christian Liebel <christian.liebel@thinktecture.com>
2021-06-02 17:04:56 +0200

    feat(exit): suppress beforeunload handler when exiting

--- src/menus/file/exit.ts
+++ src/menus/file/exit.ts
@@ -6,6 +6,10 @@ export class ExitAction implements MenuAction {
   async execute(drawingContext: DrawingContext): Promise<void> {
     try {
       await handleUnsavedChanges(drawingContext);
+
+      // suppress beforeunload handler
+      drawingContext.document.dirty = false;
+
       window.close();
     } catch {
       // Silently catch any errors
