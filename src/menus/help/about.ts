a2d503df6fcf7065eeec2ccac40a312920eeb8f8
Christian Liebel <christian.liebel@thinktecture.com>
2021-06-03 13:31:13 +0200

    refactor(dialog): add showDialog helper method

--- src/menus/help/about.ts
+++ src/menus/help/about.ts
@@ -1,13 +1,8 @@
+import { showDialog } from '../../helpers/dialog';
 import type { MenuAction } from '../../models/menu-action';
 
 export class AboutAction implements MenuAction {
   execute(): void {
-    // TODO: probably refactor this
-    const app = document.querySelector('paint-app')?.shadowRoot;
-    const dialog = document.createElement('paint-dialog-about');
-    dialog.addEventListener('close', () => {
-      app?.removeChild(dialog);
-    });
-    app?.appendChild(dialog);
+    showDialog('paint-dialog-about');
   }
 }
--- src/menus/image/flip-rotate.ts
+++ src/menus/image/flip-rotate.ts
@@ -1,12 +1,8 @@
+import { showDialog } from '../../helpers/dialog';
 import type { MenuAction } from '../../models/menu-action';
 
 export class FlipRotateAction implements MenuAction {
   execute(): void {
-    const app = document.querySelector('paint-app')?.shadowRoot;
-    const dialog = document.createElement('paint-dialog-flip-and-rotate');
-    dialog.addEventListener('close', () => {
-      app?.removeChild(dialog);
-    });
-    app?.appendChild(dialog);
+    showDialog('paint-dialog-flip-and-rotate');
   }
 }
