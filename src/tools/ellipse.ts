c8eb312cec615c7e45a327195c43b06cb87fdd52
Christian Liebel <christian.liebel@thinktecture.com>
2022-03-12 23:28:57 +0100

    feat(tools): implement ellipse tool
    Fixes #27

--- src/tools/ellipse.ts
+++ src/tools/ellipse.ts
@@ -3,53 +3,96 @@ import { clearContext } from '../helpers/clear-context';
 import type { DrawingContext } from '../models/drawing-context';
 import type { Point } from '../models/point';
 import type { ToolColor, Tool } from '../models/tool';
+import type { FillStyle } from '../models/fill-style';
 
 export class EllipseTool implements Tool {
   private startPosition: Point = { x: 0, y: 0 };
 
-  onPointerDown(
-    x: number,
-    y: number,
-    { previewContext, context }: DrawingContext,
-    color: ToolColor,
-  ): void {
-    if (context && previewContext) {
-      this.startPosition = { x, y };
-      context.fillStyle = previewContext.fillStyle = color.stroke.value;
-    }
+  onPointerDown(x: number, y: number): void {
+    this.startPosition = { x, y };
   }
 
   onPointerMove(
     x: number,
     y: number,
-    { canvas, previewContext }: DrawingContext,
+    { fillStyle, canvas, previewContext }: DrawingContext,
+    color: ToolColor,
   ): void {
     if (canvas && previewContext) {
-      this.drawEllipse(x, y, canvas, previewContext, previewContext);
+      this.drawEllipse(
+        x,
+        y,
+        fillStyle,
+        color,
+        canvas,
+        previewContext,
+        previewContext,
+      );
     }
   }
 
   onPointerUp(
     x: number,
     y: number,
-    { canvas, context, previewContext }: DrawingContext,
+    { fillStyle, canvas, context, previewContext }: DrawingContext,
+    color: ToolColor,
   ): void {
     if (canvas && context && previewContext) {
-      this.drawEllipse(x, y, canvas, context, previewContext);
+      this.drawEllipse(x, y, fillStyle, color, canvas, context, previewContext);
     }
   }
 
-  drawEllipse(
+  private drawEllipse(
     x: number,
     y: number,
+    fillStyle: FillStyle,
+    color: ToolColor,
     canvas: HTMLCanvasElement,
     targetContext: CanvasRenderingContext2D,
     previewContext: CanvasRenderingContext2D,
   ): void {
-    // TODO: Fill styles
     clearContext(previewContext);
+
+    const ellipsePixels: Point[] = [];
     ellipseRect(this.startPosition.x, this.startPosition.y, x, y, (x, y) => {
-      targetContext.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+      ellipsePixels.push({ x, y });
     });
+
+    if (fillStyle.fill) {
+      targetContext.fillStyle = color.fill.value;
+      ellipsePixels.sort((a, b) => a.y - b.y || a.x - b.x);
+      const fillPixels = this.getFillPixels(ellipsePixels);
+      Array.from(fillPixels).forEach((pixel) => {
+        this.drawPixel(targetContext, pixel);
+      });
+    }
+
+    if (fillStyle.stroke) {
+      targetContext.fillStyle = color.stroke.value;
+    }
+
+    ellipsePixels.forEach((pixel) => {
+      this.drawPixel(targetContext, pixel);
+    });
+  }
+
+  private drawPixel(context: CanvasRenderingContext2D, { x, y }: Point) {
+    context.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+  }
+
+  private *getFillPixels(pixels: Point[]): Generator<Point> {
+    let previousPixel: Point | undefined;
+
+    for (const pixel of pixels) {
+      if (previousPixel?.y === pixel.y && pixel.x - previousPixel.x > 1) {
+        const minX = Math.min(previousPixel.x, pixel.x);
+        const maxX = Math.max(previousPixel.x, pixel.x);
+        for (let i = minX; i <= maxX; i++) {
+          yield { x: i, y: pixel.y };
+        }
+      }
+
+      previousPixel = pixel;
+    }
   }
 }
