285bf9ee4fb29845831ceabdf8ead5f72b91203c
Christian Liebel <christian.liebel@thinktecture.com>
2024-05-16 16:20:10 +0200

    refactor: remove obsolete types

--- types/static.d.ts
+++ types/static.d.ts
@@ -1,63 +1,3 @@
-/* Use this file to declare any custom file extensions for importing */
-/* Use this folder to also add/extend a package d.ts file, if needed. */
-
-/* CSS MODULES */
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.module.sass' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.module.less' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-declare module '*.module.styl' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
-
-/* CSS */
-declare module '*.css';
-declare module '*.scss';
-declare module '*.sass';
-declare module '*.less';
-declare module '*.styl';
-
-/* IMAGES */
-declare module '*.svg' {
-  const ref: string;
-  export default ref;
-}
-declare module '*.bmp' {
-  const ref: string;
-  export default ref;
-}
-declare module '*.gif' {
-  const ref: string;
-  export default ref;
-}
-declare module '*.jpg' {
-  const ref: string;
-  export default ref;
-}
-declare module '*.jpeg' {
-  const ref: string;
-  export default ref;
-}
-declare module '*.png' {
-  const ref: string;
-  export default ref;
-}
-
-/* CUSTOM: ADD YOUR OWN HERE */
-
 declare module 'bresenham-zingl' {
   export function line(x0: number, y0: number, x1: number, y1: number, setPixel: (x: number, y: number) => unknown);
   export function ellipseRect(x0: number, y0: number, x1: number, y1: number, setPixel: (x: number, y: number) => unknown);
@@ -76,15 +16,6 @@ interface LaunchQueue {
 interface Window {
   launchQueue: LaunchQueue;
   queryLocalFonts: () => Promise<{ family: string }[]>;
-}
-
-// Type declarations for Web Share API + Files ("Level 2")
-interface ShareData {
-  files?: File[];
-}
-
-interface Navigator {
-  canShare?: (data?: ShareData) => boolean;
 }
 
 // Type declarations for Local Font Access API
