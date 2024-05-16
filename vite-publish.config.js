053debdffc4f536119122cff2765082bbc81d8a5
Christian Liebel <christian.liebel@thinktecture.com>
2024-05-16 19:12:11 +0200

    build: reintroduce npm package build

---
+++ vite-publish.config.js
@@ -0,0 +1,10 @@
+export default {
+    build: {
+        lib: {
+            entry: 'src/elements/index.ts',
+            fileName: 'index',
+            formats: ['es'],
+        },
+        outDir: 'dist/dist/elements/out',
+    },
+};
