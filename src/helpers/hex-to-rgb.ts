b872f7f94dde93f5479c9b43b7a9ab41616e000e
Christian Liebel <christian.liebel@thinktecture.com>
2026-01-20 08:59:15 +0100

    feat(tools): implement color eraser

---
+++ src/helpers/hex-to-rgb.ts
@@ -0,0 +1,6 @@
+export function hexToRgb(hex: string) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return { r, g, b };
+}
